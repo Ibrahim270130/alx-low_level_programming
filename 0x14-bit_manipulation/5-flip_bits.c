092a43c82b1f2d2e2b8bc2b3b8f02487ec25334c
Ibrahim270130 <opajobiolakunle@gmail.com>
2022-10-27 19:15:24 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,20 +1,22 @@
+#include "main.h"
+
 /**
- * flip_bits - A function that gets number of bits needed to flip
- * @n: How many bit flips are needed to equal m for n
- * @m: The number to set other equal
- * Return: The number of fliped bits
+ * flip_bits - returns the number of bits you would
+ * need to flip to get from oner number to another.
+ *
+ * @n: first number.
+ * @m: seconde number.
+ * Return: number of bits to be flipped.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int flips = 0;
-	unsigned long int xor = (n ^ m);
-	unsigned long int max = 0x01;
+	unsigned long int i = n ^ m;
+	unsigned int counter = 0;
 
-	while (max <= xor)
+	while (i > 0)
 	{
-		if (max & xor)
-			flips++;
-		max <<= 1;
+		counter++;
+		i &= (i - 1);
 	}
-	return (flips);
+	return (counter);
 }
